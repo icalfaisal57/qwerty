b8c60dda1098ebbc88cf282139b22bc7e1380dd5
icalfaisal57 <icalfaisal57@gmail.com>
2024-05-13 14:03:05 +0700

    qwert

---
+++ kendaraan/kendaraan.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengecekan Kebersihan Ruangan</title>
+
+    <!--css here -->
+    <link id='global-styles-inline-css' type='text/css' rel="stylesheet" href="../assets/css/form.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <link rel='stylesheet' id='hfe-style-css' href='https://multitaskingtraining.com/wp-content/plugins/header-footer-elementor/assets/css/header-footer-elementor.css?ver=1.6.24' type='text/css' media='all' />
+    <link rel='stylesheet' id='elementor-icons-css' href='https://multitaskingtraining.com/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.9.1' type='text/css' media='all' />
+    <link rel='stylesheet' id='elementor-animations-css' href='https://multitaskingtraining.com/wp-content/plugins/elementor/assets/lib/animations/animations.min.css?ver=3.0.15' type='text/css' media='all' />
+    <link rel='stylesheet' id='elementor-frontend-legacy-css' href='https://multitaskingtraining.com/wp-content/plugins/elementor/assets/css/frontend-legacy.min.css?ver=3.0.15' type='text/css' media='all' />
+    <link rel='stylesheet' id='elementor-frontend-css' href='https://multitaskingtraining.com/wp-content/plugins/elementor/assets/css/frontend.min.css?ver=3.0.15' type='text/css' media='all' />
+    <link rel="icon" href="https://multitaskingtraining.com/wp-content/uploads/2024/02/cropped-1-copyef-32x32.png" sizes="32x32" />
+    <link rel="icon" href="https://multitaskingtraining.com/wp-content/uploads/2024/02/cropped-1-copyef-192x192.png" sizes="192x192" />
+    <link rel='stylesheet' id='elementor-pro-css' href='https://multitaskingtraining.com/wp-content/plugins/elementor-pro/assets/css/frontend.min.css?ver=3.0.9' type='text/css' media='all' />
+    <link rel='stylesheet' id='she-header-style-css' href='https://multitaskingtraining.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/css/she-header-style.css?ver=1.6.10' type='text/css' media='all' />
+    <link rel='stylesheet' id='landingpress-css' href='https://multitaskingtraining.com/wp-content/themes/landingpress-wp/style.css?ver=3.4.2' type='text/css' media='all' />
+    <link rel='stylesheet' id='google-fonts-1-css' href='https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CMontserrat%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&#038;ver=6.5.2' type='text/css' media='all' />
+    <script type="text/javascript" src="https://multitaskingtraining.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+    <script type="text/javascript" src="https://multitaskingtraining.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js"></script>
+    <script type="text/javascript" src="https://multitaskingtraining.com/wp-content/plugins/sticky-header-effects-for-elementor/assets/js/she-header.js?ver=1.6.10" id="she-header-js"></script>
+    <link rel="canonical" href="https://multitaskingtraining.com/moderngais/" />
+    <link rel='shortlink' href='https://multitaskingtraining.com/?p=964' />
+    <link rel="apple-touch-icon" href="https://multitaskingtraining.com/wp-content/uploads/2024/02/cropped-1-copyef-180x180.png" />
+    <meta name="msapplication-TileImage" content="https://multitaskingtraining.com/wp-content/uploads/2024/02/cropped-1-copyef-270x270.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+</head>
+
+<body class="body">
+    <div class="container">
+        <section class="header" data-id="4aed722" data-element_type="section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+            <div class=""></div>
+            <div class="">
+                <div class="elementor-row">
+                    <div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-65f922f" data-id="65f922f" data-element_type="column">
+                        <div class="elementor-column-wrap">
+                            <div class="elementor-widget-wrap">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-17ff6a9" data-id="17ff6a9" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+                        <div class="elementor-column-wrap elementor-element-populated">
+                            <div class="elementor-widget-wrap">
+                                <div class="elementor-element elementor-element-b457f06 elementor-widget elementor-widget-image" data-id="b457f06" data-element_type="widget" data-widget_type="image.default">
+                                    <div class="elementor-widget-container">
+                                        <div class="elementor-image">
+                                            <img decoding="async" width="75" height="75" src="https://multitaskingtraining.com/wp-content/uploads/2024/02/2-3-768x768.png" class="attachment-medium_large size-medium_large" alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+
+                        <h2 class="elementor-heading-title elementor-size-default">Pengecekan Kebersihan Ruangan</h2>
+                    </div>
+                    <div class="elementor-column elementor-col-25 elementor-inner-column elementor-element elementor-element-3a28443" data-id="3a28443" data-element_type="column">
+                        <div class="elementor-column-wrap">
+                            <div class="elementor-widget-wrap">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="form">
+            <form action="https://script.google.com/macros/s/AKfycbx1gk0z9kHu-U2ZLkE9XmfXGIMcFVPifq1VNLcQSV__nXEvAK3_BZ7jcugfd2zBouGO/exec" method="post">
+                <div class="form-group">
+                    <label for="Name">Nama Driver/Petugas Checker</label>
+                    <input id="Name" name="Name" type="text" required="required" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="merk">Merek/Tipe Kendaraan (misal : Toyota Innova / Honda CRV / Toyota Avanza )</label>
+                    <input id="merk" name="merk" type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="plat">Nomor Polisi / Plat nomor kendaraan</label>
+                    <input id="plat" name="plat" type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="km">Kilometer kendaraan</label>
+                    <input id="km" name="km" type="text" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Mesin kendaraan sudah dipanaskan</label>
+                    <div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="mesin" id="mesin_0" type="radio" class="custom-control-input" value="ya" required="required">
+                            <label for="mesin_0" class="custom-control-label">ya</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="mesin" id="mesin_1" type="radio" class="custom-control-input" value="tidak" required="required">
+                            <label for="mesin_1" class="custom-control-label">tidak</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Kondisi Aki</label>
+                    <div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="aki" id="aki_0" type="radio" class="custom-control-input" value="baik" required="required">
+                            <label for="aki_0" class="custom-control-label">Berfungsi dengan Baik</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="aki" id="aki_1" type="radio" class="custom-control-input" value="rusak" required="required">
+                            <label for="aki_1" class="custom-control-label">Soak perlu diganti</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Kondisi body luar kendaraan</label>
+                    <div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="body" id="body_0" type="radio" class="custom-control-input" value="bersih" required="required">
+                            <label for="body_0" class="custom-control-label">Sudah Dibersihkan</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="body" id="body_1" type="radio" class="custom-control-input" value="baik" required="required">
+                            <label for="body_1" class="custom-control-label">Tidak perlu dibersihkan karena masih bersih</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Kondisi interior/bagian dalam kendaraan</label>
+                    <div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="interior" id="interior_0" type="radio" class="custom-control-input" value="bersih" required="required">
+                            <label for="interior_0" class="custom-control-label">Sudah Dibersihkan</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="interior" id="interior_1" type="radio" class="custom-control-input" value="baik" required="required">
+                            <label for="interior_1" class="custom-control-label">Tidak perlu dibersihkan karena masih bersih</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Pengecekan lampu-lampu kendaraan</label>
+                    <div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="lampu" id="lampu_0" type="radio" class="custom-control-input" value="baik" required="required">
+                            <label for="lampu_0" class="custom-control-label">Semua berfungsi baik</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="lampu" id="lampu_1" type="radio" class="custom-control-input" value="rusak" required="required">
+                            <label for="lampu_1" class="custom-control-label">Ada lampu yang tidak berfungsi dan perlu diganti</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Pengecekan peralatan kendaraan seperti dongkrak, kunci-kunci roda, segitiga pengaman, ban serep</label>
+                    <div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="peralatan" id="peralatan_0" type="radio" class="custom-control-input" value="lengkap">
+                            <label for="peralatan_0" class="custom-control-label">Semua alat tersedia</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="peralatan" id="peralatan_1" type="radio" class="custom-control-input" value="tidak lengkap">
+                            <label for="peralatan_1" class="custom-control-label">Ada alat yang hilang dan perlu diganti</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Pengecekan Air Radiator, Oli Mesin, Oli Rem</label>
+                    <div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="oli" id="oli_0" type="radio" class="custom-control-input" value="lengkap" required="required">
+                            <label for="oli_0" class="custom-control-label">Semua cairan Air Radiator, Oli Mesin, Oli Rem cukup tersedia</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input name="oli" id="oli_1" type="radio" class="custom-control-input" value="tidak lengkap" required="required">
+                            <label for="oli_1" class="custom-control-label">Ada cairan yang perlu ditambah</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="kerusakan">Mohon dilaporkan apabila ada kerusakan pada kendaraan, silahkan ketik dibawah ini untuk memberikan laporannya (jika ada)</label>
+                    <input id="kerusakan" name="kerusakan" type="text" class="form-control">
+                </div>
+                <div class="">
+                    <button name="submit" type="submit" class="elementor-button">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
